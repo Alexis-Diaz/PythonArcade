0dc58ecd8245dc6b5fef73957c2de90a94cf9980
Osmin Alexis <alexisdiaz4556@outlook.com>
2026-06-15 11:51:45 -0600

    Preparado para Render

--- app.py
+++ app.py
@@ -157,10 +157,13 @@ def intentar():
 # INICIO DEL SERVIDOR
 # ==========================================
 
-if __name__ == "__main__":
+""" if __name__ == "__main__":
     #app.run(debug=True)
     app.run(
     host="0.0.0.0",
     port=5000,
     debug=True
-)
+) """
+
+if __name__ == "__main__":
+    app.run()
